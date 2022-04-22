b92955987a3d1cc5970a71daeb0c8f6dc5047b9e
Deminosa <daniel@noennecke.de>
2022-04-22 19:38:05 +0200

    Command Structure deleted!

--- Core-Yuki/src/de/deminosa/core/api/YukiManager.java
+++ Core-Yuki/src/de/deminosa/core/api/YukiManager.java
@@ -1,10 +1,6 @@
 package de.deminosa.core.api;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import de.deminosa.core.exeptions.YukiLibException;
-import de.deminosa.core.manager.command.Command;
 
 /*
 *	Class Create by Deminosa
@@ -16,21 +12,10 @@ import de.deminosa.core.manager.command.Command;
 
 public class YukiManager {
 
-	private final HashMap<String, Command> commands;
 	private final ArrayList<UserLoader> userLoader;
 	
 	public YukiManager() {
-		commands = new HashMap<>();
 		userLoader = new ArrayList<>();
-	}
-	
-	public void registerCommand(String name, Command command) {
-		if(!commands.containsKey(name)) {
-			commands.put(name, command);
-		}else {
-			throw new YukiLibException("The Command '" + name + "' with class '" + command.getClass().getName() + "'"
-					+ " is already register!");
-		}
 	}
 	
 	public void registerUserLoader(UserLoader loader) {
